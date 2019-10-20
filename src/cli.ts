327c840c144802c9701b2a78d1a724e26b794037
Alex Holden <a@lexholden.com>
2019-10-20 00:25:20 -0300

    Cleanup

--- src/cli.ts
+++ src/cli.ts
@@ -15,24 +15,24 @@ const BUFFER_PATH = join(__dirname, '..', 'test', 'buffers', `${FILE}.json`)
 
 Logger.log(`ABOUT TO CAPTURE ${HTML_PATH} => ${IMG_PATH}`);
 
-(async () => {
+(async() => {
   let browser
   try {
     browser = await puppeteer.launch({
       args: ['--no-sandbox'],
       handleNodeExit: [],
-    });
+    })
     const browserVersion = await browser.version()
     Logger.log(`started ${browserVersion}`)
-    const page = await browser.newPage();
-    page.on('console', msg => Logger.log(['PAGE LOG:', msg.text()]));
+    const page = await browser.newPage()
+    page.on('console', msg => Logger.log(['PAGE LOG:', msg.text()]))
     // // const stuff = await page.goto('http://google.com', { waitUntil: 'networkidle2' });
-    const stuff = await page.goto(`file://${ HTML_PATH }`, { waitUntil: 'networkidle2' });
+    const stuff = await page.goto(`file://${ HTML_PATH }`, { waitUntil: 'networkidle2' })
     const screen = await page.screenshot({ path: IMG_PATH })
     writeFileSync(BUFFER_PATH, JSON.stringify(screen), { encoding: 'binary' })
     // Logger.log(screen)
     // console.log(screen.length, screen.toJSON())
-    return browser.close();
+    return browser.close()
   } catch (err) {
     // console.log(err)
     Logger.log(err)
@@ -42,6 +42,6 @@ Logger.log(`ABOUT TO CAPTURE ${HTML_PATH} => ${IMG_PATH}`);
 })().then(d => {
   Logger.log('DONE, EXITING', d)
   process.exit()
-});
+})
 
 process.stdin.resume()
